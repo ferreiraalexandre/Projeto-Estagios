889d11db4c25499d2a1b51ac57369fb64f1e16ca
alexa <alexa@172.20.79.59>
2017-03-29 21:07:50 -0300

    DEV-Criação da grid no menu inicial. Parte 2

--- src/main/webapp/projeto/js/module.js
+++ src/main/webapp/projeto/js/module.js
@@ -1,4 +1,4 @@
-var app = angular.module("projeto-estagios", []);
+var app = angular.module("projeto-estagios", ['ngGrid']);
 
 app.factory('service', function() {
 	var valor = {};
--- src/main/webapp/projeto/menu/menu.js
+++ src/main/webapp/projeto/menu/menu.js
@@ -1,11 +1,14 @@
-var app =app.controller('MyCtrl', function($scope) {
-    $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];
-    $scope.gridOptions = { 
-        data: 'myData',
-        columnDefs: [{field:'name', displayName:'Name'}, {field:'age', displayName:'Age'}]
-    };
+
+'use strict';
+
+//var app = angular.module('myApp', ['ngGrid']);
+app.controller('projetoController', function($scope) {
+  $scope.myData = [
+  {name: "Moroni", age: 50},
+  {name: "Tiancum", age: 43},
+  {name: "Jacob", age: 27},
+  {name: "Nephi", age: 29},
+  {name: "Enos", age: 34}];
+  
+  $scope.gridOptions = { data: 'myData' };
 });
