10e9553a702a41e9e8780d3f053deb2f24fec7d3
alexa <alexa@172.20.79.27>
2017-04-05 22:12:45 -0300

    merge

--- src/main/webapp/projeto/js/module.js
+++ src/main/webapp/projeto/js/module.js
@@ -1,4 +1,4 @@
-var app = angular.module("projeto-estagios", ['ngMaterial', 'md.data.table']);
+var app = angular.module("projeto-estagios", ['ngMaterial', 'md.data.table', 'ngMdIcons']);
 
 app.factory('service', function() {
 	var valor = {};
