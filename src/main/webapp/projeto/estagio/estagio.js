71a80d83eb17e4885b9f410d9ecb36c8f45be570
alexa <alexa@172.20.79.133>
2017-04-25 18:54:12 -0300

    merge

--- src/main/webapp/projeto/estagio/estagio.js
+++ src/main/webapp/projeto/estagio/estagio.js
@@ -1,13 +1,10 @@
 
-
 app.config(['$mdThemingProvider', '$mdIconProvider' , function ($mdThemingProvider) {
     'use strict';
     
     $mdThemingProvider.theme('default')
       .primaryPalette('blue');
 }])
-
-
 
 app.controller('estagioController', ['$mdEditDialog', '$q', '$scope', '$timeout', '$mdDialog','$location', function ($mdEditDialog, $q, $scope, $timeout, $mdDialog, $location) {
   'use strict';
