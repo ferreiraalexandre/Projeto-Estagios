23f1cd2b3e78ea672e60ec73e7967dd276381ba3
jeronimoflima <jeronimoflima@yahoo.com.br>
2017-05-31 20:25:14 -0300

    DEV

--- src/main/webapp/projeto/service/services.js
+++ src/main/webapp/projeto/service/services.js
@@ -4,10 +4,10 @@ app.factory('UsuarioService',['$resource',  function ($resource) {
 		  postUsuario: { method: 'POST', params: {method: 'salva'}}, 
 		  getList: 	{ method: 'GET', params: {method: 'buscar'}, isArray: false },
 		  putUsuario: { method: 'PUT', params: {method: 'editar'}},
-		  deleteUsuario: { method: 'DELETE', params: {method: 'deletar'}, isArray: false, interceptor: {responseError : resultError}}
+		  deleteUsuario: { method: 'DELETE', params: {method: 'deletar'}, isArray: false}
 
 	  })
-	}]);
+}]);
 
 app.factory('UnidadeEnsinoService',['$resource',  function ($resource) {
 	  return $resource(app.pathRest + '/unidadeEnsino/:method/:data', {}, {
@@ -15,6 +15,36 @@ app.factory('UnidadeEnsinoService',['$resource',  function ($resource) {
 		  getList: {method: 'GET', params: {method: 'buscar'}, isArray: false},
 		  putUnidade: { method: 'PUT', params: {method: 'editar'}},
 		  deleteUnidade: { method: 'DELETE', params: {method: 'deletar'}, isArray: false, interceptor: {responseError : resultError}}
+	  })
+}]);
+
+app.factory('EmpresaService',['$resource',  function ($resource) {
+	  return $resource(app.pathRest + '/empresa/:method/:data', {}, {
+		  postEmpresa: { method: 'POST', params: {method: 'salva'}}, 
+		  getList: {method: 'GET', params: {method: 'buscar'}, isArray: false},
+		  putEmpresa: { method: 'PUT', params: {method: 'editar'}},
+		  deleteEmpresa: { method: 'DELETE', params: {method: 'deletar'}, isArray: false, interceptor: {responseError : resultError}}
+		   
+
+	  })
+}]);
+
+app.factory('TurmaService',['$resource',  function ($resource) {
+	  return $resource(app.pathRest + '/turma/:method/:data', {}, {
+		  postTurma: { method: 'POST', params: {method: 'salva'}}, 
+		  getList: {method: 'GET', params: {method: 'buscar'}, isArray: false},
+		  putTurma: { method: 'PUT', params: {method: 'editar'}},
+		  deleteTurma: { method: 'DELETE', params: {method: 'deletar'}, isArray: false, interceptor: {responseError : resultError}}
+	  })
+}]);
+
+
+app.factory('EmpresaService',['$resource',  function ($resource) {
+	  return $resource(app.pathRest + '/empresa/:method/:data', {}, {
+		  postEmpresa: { method: 'POST', params: {method: 'salva'}}, 
+		  getList: {method: 'GET', params: {method: 'buscar'}, isArray: false},
+		  putEmpresa: { method: 'PUT', params: {method: 'editar'}},
+		  deleteEmpresa: { method: 'DELETE', params: {method: 'deletar'}, isArray: false, interceptor: {responseError : resultError}}
 		   
 
 	  })
