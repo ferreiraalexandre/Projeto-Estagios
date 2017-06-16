30d69afd6348c3f382a12556f0ffe0ba0ea52e24
Alexandre Ferreira <alexandre.ferreira@gesplan.com.br>
2017-06-16 12:42:51 -0300

    merge

--- src/main/java/br/com/projetoEstagio/connection/JPAConnection.java
+++ src/main/java/br/com/projetoEstagio/connection/JPAConnection.java
@@ -6,35 +6,7 @@ import javax.persistence.Persistence;
 
 public class JPAConnection {
 
-	private static EntityManagerFactory conection;
-	private static EntityManager em;
-	
-	private EntityManagerFactory connect(){
-		
-		try{
-			if(conection != null && conection.isOpen()){
-				return conection;
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		conection = Persistence.createEntityManagerFactory("projetoEstagio");
-		
-		return conection;
-	}
-	
 	protected EntityManager getEntityManager(){
-		if(em == null || !em.isOpen()){
-			em = connect().createEntityManager();
-		}
-		return em;
-	}
-	
-	protected void closeConection(){
-		
-		if(em != null && em.isOpen()){
-			em.close();
-		}
+		return EntityManagerUtil.getEMIntance();
 	}
 }
