3aa6f14f6b85cce50d276ed8e8628896cd32d14b
Alexandre Ferreira <alexandre.ferreira@gesplan.com.br>
2017-07-28 13:30:15 -0300

    Melhorias

--- src/main/webapp/projeto/estagio/estagio.js
+++ src/main/webapp/projeto/estagio/estagio.js
@@ -98,6 +98,25 @@ app.controller('estagioController', ['$mdEditDialog', '$q', '$scope', '$timeout'
 		});		
 	};
 
+	
+	  $scope.filtrarData = function(ev) {
+		    // Appending dialog to document.body to cover sidenav in docs app
+		    var confirm = $mdDialog.prompt()
+		      .title('What would you name your dog?')
+		      .textContent('Bowser is a common name.')
+		      .placeholder('Dog name')
+		      .ariaLabel('Dog name')
+		      .initialValue('Buddy')
+		      .targetEvent(ev)
+		      .ok('Okay!')
+		      .cancel('I\'m a cat person');
+
+		    $mdDialog.show(confirm).then(function(result) {
+		      $scope.status = 'You decided to name your dog ' + result + '.';
+		    }, function() {
+		      $scope.status = 'You didn\'t name your dog.';
+		    });
+		  };
 
 	//Chama função para buscar estagios
 	$scope.getEstagio();
