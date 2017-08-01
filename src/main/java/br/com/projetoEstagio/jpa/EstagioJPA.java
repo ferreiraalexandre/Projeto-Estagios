8dd2e5384f1a6c69060018f1df6d42dfb901f916
Alexandre Ferreira <alexandre.ferreira@gesplan.com.br>
2017-08-01 13:49:08 -0300

    DEV- Filtro Estagio

--- src/main/java/br/com/projetoEstagio/jpa/EstagioJPA.java
+++ src/main/java/br/com/projetoEstagio/jpa/EstagioJPA.java
@@ -1,7 +1,9 @@
 package br.com.projetoEstagio.jpa;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.com.projetoEstagio.entity.Empresa;
 import br.com.projetoEstagio.entity.Estagio;
@@ -28,12 +30,17 @@ public class EstagioJPA extends JPAAbstract<Estagio, Long> implements EstagioInt
 	}
 	
 	public List<Estagio> filtrar(Date dataInicio, Date dataFim, Long turmaId) {
-		String hql = "SELECT E FROM "+ this.getEntityName() +" E WHERE E.turma_id = "+ turmaId;
-//		if(turmaId != null){
-//			hql += " AND E.turmaId = '"+ turmaId +"'";
-//			
-//		}
-		return this.list("SELECT E FROM "+ this.getEntityName() +" E WHERE E.turmaId = '"+ turmaId + "'");
+		String hql = "SELECT E FROM "+ this.getEntityName() +" E WHERE E.dataFim BETWEEN :dataInicio AND :dataFim";
+		
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("dataInicio", dataInicio);
+		params.put("dataFim", dataFim);
+		return findByQuery(hql, params);
+		
+
+	
 
 	}	
 
